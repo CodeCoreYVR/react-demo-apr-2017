0768b7a87a73c97ba5afb1ffdebb020ca3943aa0
Steve Godin <steve.god@gmail.com>
2017-06-22 15:41:37 -0700

    Add solutions to orange rectangle & type prop

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@
 import React from 'react';
 import HelloWorld from './HelloWorld';
 import Shape from './Shape';
+import OrangeRectangle from './OrangeRectangle';
 
 function App (props) {
   return (
@@ -12,6 +13,11 @@ function App (props) {
       <HelloWorld name='Duncan McLeod' />
       <HelloWorld name='Rick' />
       <Shape />
+      <OrangeRectangle />
+      <Shape color='salmon' />
+      <Shape color='aqua' />
+      <Shape type='circle' color='violet' />
+      <Shape type='diamond' color='maroon' />
     </div>
   ) // make sure to name your components in CamelCase and
   // always call them in JSX as such. Using lower case in
--- src/components/Shape.js
+++ src/components/Shape.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
 function Shape (props) {
+  const style = {
+    backgroundColor: 'blue',
+    height: '30px',
+    width: '30px'
+  };
+  
+  style.backgroundColor = props.color || 'blue';
+  if (props.type == 'circle') {
+    style.borderRadius = '999999px';
+  }
+  if (props.type == 'diamond') {
+    style.transform = 'rotateZ(45deg) scale(0.8)';
+  }
+
   return (
     <div style={style} className='shape' />
   );
 }
 
-const style = {
-  backgroundColor: 'blue',
-  height: '30px',
-  width: '30px'
-};
 
 export default Shape;
